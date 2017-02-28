f16ff16d3dc44993f66220822aea3bfbb38c17da
davidpodhola <david.podhola@naseukoly.cz>
2017-02-28 22:28:35 +0100

    wip2

--- src/webparts/helloWorld/components/HelloWorld.tsx
+++ src/webparts/helloWorld/components/HelloWorld.tsx
@@ -2,13 +2,12 @@ import * as React from 'react';
 import styles from './HelloWorld.module.scss';
 import { IHelloWorldProps } from './IHelloWorldProps';
 import { escape } from '@microsoft/sp-lodash-subset';
-import { TodoApp } from '../../../Client/out/bundle';
+import * as bundle from '../../../Client/out/bundle';
 
 export default class HelloWorld extends React.Component<IHelloWorldProps, void> {
   public render(): React.ReactElement<IHelloWorldProps> {
-
     return (
-      <TodoApp />
+      <bundle.TodoApp />
     );
   }
 }
