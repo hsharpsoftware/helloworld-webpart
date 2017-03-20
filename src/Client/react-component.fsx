82f5b80762f4eb89e335356d710afcf83d610155
davidpodhola <david.podhola@naseukoly.cz>
2017-03-20 20:23:14 +0100

    WIP

--- src/Client/react-component.fsx
+++ src/Client/react-component.fsx
@@ -1,11 +1,14 @@
 
 #r "../../node_modules/fable-core/Fable.Core.dll"
 #r "../../node_modules/fable-react/Fable.React.dll"
+#r "../../node_modules/fable-import-sp-pnp-js/fable-import-sp-pnp-js.dll"
+#r "../../node_modules/fable-powerpack/Fable.PowerPack.dll"
 
 open System
 open Fable.Core
 open Fable.Core.JsInterop
 open Fable.Import
+open Fable.PowerPack
 
 module R = Fable.Helpers.React
 open R.Props
@@ -14,10 +17,24 @@ type TodoApp(props) =
     inherit React.Component<obj,obj>(props)
     do base.setInitState()
 
+    let mutable web: sharepoint.webs.Web = null
+
+    let getWeb () =
+        async {            
+            let! _web = 
+                (pnp.Globals.sp.web.select( [|"Title"|] ) :> sharepoint.queryable.Queryable).get()  
+                |> Promise.map( fun w -> (w :?> sharepoint.webs.Web ) )              
+                |> Async.AwaitPromise
+
+            web <- _web
+        } |> Async.StartImmediate
+
     member this.render () =
+        getWeb()
+
         R.div [] [
             R.header [ ClassName "header" ] [
-                R.h1 [] [ R.str "todos" ]
+                R.h1 [] [ R.str (sprintf "todos %A" web?Title ) ]
                 R.input [
                     ClassName "new-todo"
                     Placeholder "What needs to be done?"
